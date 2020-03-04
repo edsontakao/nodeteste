c49f597c54e0bed46fff5849f48520c5c4a9f44f
ekawahara <edsontakao@gmail.com>
2020-03-03 22:24:51 -0300

    express

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const http = require('http');
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World Takao</h1>');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello Express World!');
 });
 
-server.listen(port,() => {
+
+app.listen(port,() => {
   console.log(`Server running at port `+port);
 });
-
